f870e8620ddffc31e613fee6577211d8f9ea0bde
Kevin Laeufer <ekiwi@berkeley.edu>
2017-09-21 23:50:20 -0700

    [mutation] simple bitflip

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::env;
 use std::ffi::{CString};
 use run::{TestRunner};
 use run::afl::{AflConfig, AflRunner};
-
+use mutation::{BitFlip};
 
 // from `afl/config.h`
 pub const MAP_SIZE: usize = 1 << 16;
@@ -18,18 +18,24 @@ fn main() {
 
 	// TODO: extract from command line args
 	let argv = &["./put/build-zlib/minigzip", "-d"];
-	let input = b"\x00\x00\x00\x00\x00\x00\x00\x00";
-	//let input = b"\x1f\x8b\x08\xfb";
+	let mut input = [0u8; 8];
 
 	let cfg = AflConfig {argv : argv, map_size : MAP_SIZE, mem_limit : MEM_LIMIT};
 	let runner = AflRunner::create(&cfg);
-	let runs = 1000 as i64;
-	println!("running {} {} times with the same input...", argv[0], runs);
+
+	let mutator = BitFlip::create(1, input.len());
+
+	let runs = mutator.max;
+	println!("running {} {} times simple input mutation", argv[0], runs);
 	let start = time::PreciseTime::now();
-	for _ in 0..runs {
-		runner.run(input);
+	for mutation in mutator {
+		mutation.run(&mut input);
+		println!("{:?}", input);
+		runner.run(&input);
+		// reset input
+		input = [0u8; 8];
 	}
-	let duration = start.to(time::PreciseTime::now());
-	let runs_per_second = (runs * 1000 * 1000) as f64 / duration.num_microseconds().unwrap() as f64;
+	let duration = start.to(time::PreciseTime::now()).num_microseconds().unwrap();
+	let runs_per_second = (runs * 1000 * 1000) as f64 / duration as f64;
 	println!("{:.1} runs/s", runs_per_second);
 }
--- src/mutation.rs
+++ src/mutation.rs
@@ -1,12 +1,39 @@
-
-
-struct BitFlip {
-	bit_count : u32,
+pub struct BitFlip {
+	bit_count: u32,
+	input_size: usize,
+	ii: usize,
+	pub max: usize,
 }
 
 impl BitFlip {
+	pub fn create(bit_count: u32, input_size: usize) -> BitFlip {
+		let ii = 0;
+		let max = input_size * 8 - (bit_count as usize) + 1;
+		BitFlip { bit_count, input_size, ii, max }
+	}
+}
 
-	// fn mutate(input : &u8, ) {
+impl Iterator for BitFlip {
+	type Item = Mutator;
+	fn next(&mut self) -> Option<Mutator> {
+		if self.ii < self.max {
+			let ii = self.ii;
+			self.ii += 1;
+			Some(Mutator { ii })
+		} else {
+			None
+		}
+	}
+}
 
-	// }
+pub struct Mutator {
+	ii: usize
+}
+
+impl Mutator {
+	pub fn run(&self, input : &mut [u8]) {
+		let byte = self.ii / 8;
+		let bit = self.ii - (byte * 8);
+		input[byte] ^= (1 << bit) as u8;
+	}
 }
