8fd748ffa5f5a197d93a775eaa6400250db10f5a
Kevin Laeufer <ekiwi@berkeley.edu>
2017-09-25 13:08:28 -0700

    [mutation] iterate over all available mutations

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ mod mutation;
 use std::env;
 use run::{TestRunner};
 use run::afl::{AflConfig, AflRunner};
-use mutation::{BitFlip};
+use mutation::{MUTATIONS};
 use std::hash::Hasher;
 use twox_hash::XxHash;
+use std::collections::HashSet;
 
 // from `afl/config.h`
 pub const MAP_SIZE: usize = 1 << 16;
@@ -30,24 +31,26 @@ fn main() {
 
 	let cfg = AflConfig {argv : argv, map_size : MAP_SIZE, mem_limit : MEM_LIMIT};
 	let runner = AflRunner::create(&cfg);
+	let mut runs : usize = 0;
 
-	let mutator = BitFlip::create(1, input.len());
-
-	let runs = mutator.max;
-	println!("running {} {} times simple input mutation", argv[0], runs);
 	let start = time::PreciseTime::now();
-	let mut old_hash = 0u64;
-	for mutation in mutator {
-		mutation.run(&mut input);
-		runner.run(&input);
-		// analyze
-		let new_hash = hash_xx(runner.coverage().as_slice_u8());
-		if new_hash != old_hash {
-			println!("Input generated new coverage: {:?}", input);
-			old_hash = new_hash;
+	let mut old_hashes : HashSet<u64> = HashSet::new();
+	for mutation in MUTATIONS.iter() {
+		let iterator = mutation.iter(input.len());
+		runs += iterator.max;
+		println!("running {} {} times with {} mutation", argv[0], iterator.max, mutation.name);
+		for mutator in iterator {
+			mutator.run(&mut input);
+			runner.run(&input);
+			// analyze
+			let new_hash = hash_xx(runner.coverage().as_slice_u8());
+			if !old_hashes.contains(&new_hash) {
+				println!("Input generated new coverage: {:?}", input);
+				old_hashes.insert(new_hash);
+			}
+			// reset input
+			input = [0u8; 8];
 		}
-		// reset input
-		input = [0u8; 8];
 	}
 	let duration = start.to(time::PreciseTime::now()).num_microseconds().unwrap();
 	let runs_per_second = (runs * 1000 * 1000) as f64 / duration as f64;
--- src/mutation.rs
+++ src/mutation.rs
@@ -1,25 +1,41 @@
-pub struct BitFlip {
-	bit_count: u32,
-	input_size: usize,
-	ii: usize,
-	pub max: usize,
+pub struct Mutation {
+	pub name: &'static str,
+	max:   fn(usize) -> usize,
+	apply: fn(usize, &mut [u8])
 }
 
-impl BitFlip {
-	pub fn create(bit_count: u32, input_size: usize) -> BitFlip {
-		let ii = 0;
-		let max = input_size * 8 - (bit_count as usize) + 1;
-		BitFlip { bit_count, input_size, ii, max }
+pub const MUTATIONS : [Mutation; 9] = [
+	Mutation { name: "bitflip  1/1", max: bitflip_1_max,  apply: bitflip_1 },
+	Mutation { name: "bitflip  2/1", max: bitflip_2_max,  apply: bitflip_2 },
+	Mutation { name: "bitflip  4/1", max: bitflip_4_max,  apply: bitflip_4 },
+	Mutation { name: "bitflip  8/8", max: byteflip_1_max, apply: byteflip_1 },
+	Mutation { name: "bitflip 16/8", max: byteflip_2_max, apply: byteflip_2 },
+	Mutation { name: "bitflip 32/8", max: byteflip_4_max, apply: byteflip_4 },
+	Mutation { name: "arith  8/8",   max:  arith_8_max,   apply:  arith_8 },
+	Mutation { name: "arith 16/8",   max: arith_16_max,   apply: arith_16 },
+	Mutation { name: "arith 32/8",   max: arith_32_max,   apply: arith_32 },
+];
+
+impl Mutation {
+	pub fn iter(&self, len: usize) -> MutationIterator {
+		MutationIterator { ii: 0, max: (self.max)(len), apply: self.apply }
 	}
 }
 
-impl Iterator for BitFlip {
+pub struct MutationIterator {
+	ii: usize,
+	pub max: usize,
+	apply: fn(usize, &mut [u8])
+}
+
+impl Iterator for MutationIterator {
 	type Item = Mutator;
 	fn next(&mut self) -> Option<Mutator> {
 		if self.ii < self.max {
 			let ii = self.ii;
+			let apply = self.apply;
 			self.ii += 1;
-			Some(Mutator { ii })
+			Some(Mutator { ii, apply })
 		} else {
 			None
 		}
@@ -27,14 +43,13 @@ impl Iterator for BitFlip {
 }
 
 pub struct Mutator {
-	ii: usize
+	ii: usize,
+	apply: fn(usize, &mut [u8])
 }
 
 impl Mutator {
 	pub fn run(&self, input : &mut [u8]) {
-		let byte = self.ii / 8;
-		let bit = self.ii - (byte * 8);
-		input[byte] ^= (1 << bit) as u8;
+		(self.apply)(self.ii, input)
 	}
 }
 
@@ -61,25 +76,88 @@ fn bitflip_4(ii: usize, input: &mut [u8]) {
 	input[byte] ^= (1 << (bit+3)) as u8;
 }
 // similar to AFL's bitflip 8/8
-fn byteflip_3_max(len: usize) -> usize { len }
+fn byteflip_1_max(len: usize) -> usize { len }
 fn byteflip_1(ii: usize, input: &mut [u8]) {
 	input[ii] ^= 0xff;
 }
-fn byteflip_3_max(len: usize) -> usize { len - 1 }
+fn byteflip_2_max(len: usize) -> usize { len - 1 }
 fn byteflip_2(ii: usize, input: &mut [u8]) {
 	input[ii]   ^= 0xff;
 	input[ii+1] ^= 0xff;
 }
-fn byteflip_3_max(len: usize) -> usize { len - 3 }
-fn byteflip_3(ii: usize, input: &mut [u8]) {
+fn byteflip_4_max(len: usize) -> usize { len - 3 }
+fn byteflip_4(ii: usize, input: &mut [u8]) {
 	input[ii]   ^= 0xff;
 	input[ii+1] ^= 0xff;
 	input[ii+2] ^= 0xff;
 	input[ii+3] ^= 0xff;
 }
 
-const ARITH_MAX : i32 = 35;
+const ARITH_MAX : usize = 35;
 fn arith_8_max(len: usize) -> usize { 2 * len * ARITH_MAX }
 fn arith_8(ii: usize, input: &mut [u8]) {
-	// TODO
+	let byte    = ii / ARITH_MAX / 2;
+	let rest    = ii - (byte * ARITH_MAX * 2);
+	let b = (rest / 2) as i8;
+	let a = input[byte] as i8;
+	let add_not_sub = (rest & 1) == 0;
+	input[byte] = if add_not_sub { a + b } else { a - b } as u8;
+}
+#[derive(Clone, Copy)]
+enum Endian { Big, Little }
+fn read_u16(end: Endian, mem: &[u8], byte: usize) -> u16 {
+	match end {
+		Endian::Big    => (mem[byte+0] as u16) << 8 | (mem[byte+1] as u16),
+		Endian::Little => (mem[byte+1] as u16) << 8 | (mem[byte+0] as u16),
+	}
+}
+fn write_u16(end: Endian, mem: &mut [u8], byte: usize, value: u16) {
+	let high = (value >> 8) as u8;
+	let low  = (value & 0xff) as u8;
+	match end {
+		Endian::Big    => { mem[byte+0] = high; mem[byte+1] = low; },
+		Endian::Little => { mem[byte+1] = high; mem[byte+0] = low; }
+	}
+}
+fn arith_16_max(len: usize) -> usize { 4 * (len-1) * ARITH_MAX }
+fn arith_16(ii: usize, input: &mut [u8]) {
+	let byte    = ii / ARITH_MAX / 4;
+	let rest    = ii - (byte * ARITH_MAX * 4);
+	let b = (rest / 4) as i16;
+	let add_not_sub = (rest & 2) == 0;
+	let endian = if (rest & 1) == 1 { Endian::Big } else { Endian::Little };
+	let a = read_u16(endian, input, byte) as i16;
+	let res = if add_not_sub { a + b } else { a - b} as u16;
+	write_u16(endian, input, byte, res);
+}
+fn read_u32(end: Endian, mem: &[u8], byte: usize) -> u32 {
+	match end {
+		Endian::Big    => (mem[byte+0] as u32) << 24 | (mem[byte+1] as u32) << 16 |
+		                  (mem[byte+2] as u32) <<  8 | (mem[byte+3] as u32) <<  0,
+		Endian::Little => (mem[byte+3] as u32) << 24 | (mem[byte+2] as u32) << 16 |
+		                  (mem[byte+1] as u32) <<  8 | (mem[byte+0] as u32) <<  0,
+	}
+}
+fn write_u32(end: Endian, mem: &mut [u8], byte: usize, value: u32) {
+	let byte_3 = ((value >> 24) & 0xff) as u8;
+	let byte_2 = ((value >> 16) & 0xff) as u8;
+	let byte_1 = ((value >>  8) & 0xff) as u8;
+	let byte_0 = ((value >>  0) & 0xff) as u8;
+	match end {
+		Endian::Big    => { mem[byte+0] = byte_3; mem[byte+1] = byte_2;
+		                    mem[byte+2] = byte_1; mem[byte+3] = byte_0; },
+		Endian::Little => { mem[byte+3] = byte_3; mem[byte+2] = byte_2;
+		                    mem[byte+1] = byte_1; mem[byte+0] = byte_0; },
+	}
+}
+fn arith_32_max(len: usize) -> usize { 4 * (len-3) * ARITH_MAX }
+fn arith_32(ii: usize, input: &mut [u8]) {
+	let byte    = ii / ARITH_MAX / 4;
+	let rest    = ii - (byte * ARITH_MAX * 4);
+	let b = (rest / 4) as i32;
+	let add_not_sub = (rest & 2) == 0;
+	let endian = if (rest & 1) == 1 { Endian::Big } else { Endian::Little };
+	let a = read_u32(endian, input, byte) as i32;
+	let res = if add_not_sub { a + b } else { a - b} as u32;
+	write_u32(endian, input, byte, res);
 }
