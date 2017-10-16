cf810fe655b9b04f42c996f07a84776c9eda5889
Kevin Laeufer <ekiwi@berkeley.edu>
2017-10-16 13:51:30 -0700

    [queue] some early draft of the actual queue
    FPGA Communication Queue vs Test Queue ...
    -> name conflict!

---
+++ src/queue.rs
@@ -0,0 +1,25 @@
+
+
+
+pub struct Queue {
+}
+
+
+struct Entry {
+	id: u32,
+	input: Vec<u8>,
+	fuzzing_stage: u8,
+
+
+
+}
+
+impl Queue {
+	/// creates a new queue if filename points to empty directory
+	pub fn open(filename: &str) -> Self {
+	}
+
+	fn add_test(&mut self) -> u64 {
+
+	}
+}
