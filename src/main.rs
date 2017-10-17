7fd70eab4fd56fda29f6eeba0eb65e4e0a56b2b4
Kevin Laeufer <ekiwi@berkeley.edu>
2017-10-17 16:37:20 -0700

    [main] print time spent sending/receiving buffers
    NOTE: since we currently only have a single buffer,
    this includes the time the fuzz server spends running
    our inputs...

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ fn main() {
 	let mut runs : usize = 0;
 	let mut test_id : u64 = 10;
 	let start = time::PreciseTime::now();
+	let mut io_time = 0 as i64;
 	let mut analysis = Analysis::new(TEST_SIZE.coverage);
 	let mut input = orig_input.clone();
 
@@ -38,9 +39,11 @@ fn main() {
 			mutator.run(&mut input);
 			// try to add test to buffer, handle full buffer
 			if test_buf.add_test(test_id, &input).is_err() {
+				let io_start = time::PreciseTime::now();
 				server.push_buffer(test_buf, cov_buf);
 				// TODO: use second buffer
 				let (mut a, mut b) = server.pop_buffer().expect("failed to get buffer back");
+				io_time += io_start.to(time::PreciseTime::now()).num_microseconds().unwrap();
 				test_buf = a;
 				cov_buf = b;
 				// analyze coverage
@@ -55,12 +58,16 @@ fn main() {
 		}
 	}
 	if !test_buf.is_empty() {
+		let io_start = time::PreciseTime::now();
 		server.push_buffer(test_buf, cov_buf);
 		let (_, _) = server.pop_buffer().expect("failed to get buffer back");
+		io_time += io_start.to(time::PreciseTime::now()).num_microseconds().unwrap();
 	}
 	let duration = start.to(time::PreciseTime::now()).num_microseconds().unwrap();
 	let runs_per_second = (runs * 1000 * 1000) as f64 / duration as f64;
 	println!("{:.1} runs/s ({} tests total)", runs_per_second, runs);
 	println!("Discovered {} new paths.", analysis.path_count());
 	println!("Discovered {} new inputs.", analysis.new_inputs_count());
+	let io_ratio = io_time as f64 / duration as f64;
+	println!("I/O time: {} ms ({:.2}%)", io_time as f64 / 1000 as f64, io_ratio * 100.);
 }
