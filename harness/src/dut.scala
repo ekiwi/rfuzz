161f3bfd84a2cfa940b59ea2db1fde84b17c199d
Kevin Laeufer <ekiwi@berkeley.edu>
2017-11-05 17:51:48 -0800

    [harness] move DUT wrapper stuff into separate file

---
+++ harness/src/dut.scala
@@ -0,0 +1,77 @@
+package pynq
+
+import chisel3._
+import chisel3.util._
+
+import scala.collection.immutable.ListMap
+
+object ListBundle {
+	def apply(el: ListMap[String, Data]): Record = {
+		new Record {
+			val elements: ListMap[String, Data] = el
+			override def cloneType : this.type = {
+				throw new Exception(s"Cloning not supported until someone explains to me what it's for!")
+				this
+			}
+		}
+	}
+}
+
+class CoverageConfig {
+	val counters: ListMap[String, Int] = ListMap(
+		"io_in_ready"          -> 8,
+		"io_out_valid"         -> 8,
+		"cov_valid"            -> 8,
+		"cov_bits_T_43_true"   -> 8,
+		"cov_bits_T_43_false"  -> 8,
+		"cov_bits_T_45_true"   -> 8,
+		"cov_bits_T_45_false"  -> 8,
+		"cov_bits_T_54_true"   -> 8,
+		"cov_bits_T_54_false"  -> 8,
+		"cov_bits_start_true"  -> 8,
+		"cov_bits_start_false" -> 8
+	)
+}
+
+class DUTConfig(cov: CoverageConfig) {
+	val src = "gcdcov.v"
+	val name = "gcdcov"
+	val input: ListMap[String, Int] = ListMap(
+		"io_in_valid"  ->  1,
+		"io_out_ready" ->  1,
+		"io_in_bits_a" -> 32,
+		"io_in_bits_b" -> 32)
+	val coverage_counters = cov.counters.map{case(k,_) => k}
+}
+
+class DUTBlackBox(conf: DUTConfig) extends HasBlackBoxInline {
+	override def desiredName = conf.name
+	val io = this.IO(ListBundle( {
+		ListMap("clock" -> Input(Bool()), "reset" -> Input(Bool())) ++
+		conf.input.map{ case(n,w) => n -> Input(UInt(w.W)) } ++
+		conf.coverage_counters.map{ case n => n -> Output(Bool()) }
+		// TODO: is it ok to just ignore the output?
+	}))
+	setInline(conf.src,
+		scala.io.Source.fromFile("../build/" + conf.src).mkString)
+}
+
+class DUT(conf: DUTConfig) extends Module {
+	val input_bits = conf.input.map{case(_,w) => w}.reduce(_+_)
+	val coverage_bits = conf.coverage_counters.size
+	val io = this.IO(new Bundle {
+		val inputs = Input(UInt(input_bits.W))
+		val coverage = Output(UInt(coverage_bits.W))
+	})
+	val bb = Module(new DUTBlackBox(conf))
+
+	// extract inputs
+	val pins = bb.io.elements
+	var left = input_bits - 1
+	conf.input.map{ case(n,w) =>
+		pins(n) := io.inputs(left, left - w + 1)
+		left = left - w
+	}
+	// concatenate coverage wires
+	io.coverage := Cat(conf.coverage_counters.map{ case n => pins(n).asUInt() }.toSeq)
+}
