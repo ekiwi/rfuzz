b1505e0a199a6db25c1ed5af9ec0c29968fdbdd1
Kevin Laeufer <ekiwi@berkeley.edu>
2017-11-05 20:13:39 -0800

    [harness] untested Coverage module

---
+++ harness/src/coverage.scala
@@ -0,0 +1,71 @@
+package pynq
+
+import chisel3._
+import chisel3.util._
+
+class SaturatingCounter(width : Int) extends Module {
+	val io = this.IO(new Bundle {
+		val enable = Input(Bool())
+		val value = Output(UInt(width.W))
+	})
+	val count = RegInit(0.U(width.W))
+	io.value := count
+	val max = ((1 << width) - 1).U
+	count := Mux(!io.enable || count === max, count, count + 1.U)
+}
+
+
+// 1) reset with DUT
+// 2) assert `do_collect` in first cycle after test, keep asserted!
+// 3) wait for `collect_done` to be asserted
+// 4) goto (1)
+class Coverage(conf: CoverageConfig) extends Module {
+	val out_width = 64
+	// all counter output values concatenated
+	val coverage_width = conf.counters.map{ case(n,w) => w }.reduce(_+_)
+	val output_count = div2Ceil(coverage_width, out_width)
+
+	val io = this.IO(new Bundle {
+		// from DUT
+		val coverage_signals = Input(UInt(conf.counters.size.W))
+		// internal control
+		val do_collect = Input(Bool())
+		val collect_done = Output(Bool())
+		// simple axi stream producer
+		val ready = Input(Bool())
+		val valid = Output(Bool())
+		val data  = Output(UInt(out_width.W))
+		val last  = Output(Bool())
+	})
+	val collecting = io.do_collect
+
+	val connect_coverage = !collecting
+	val coverage = {
+		var left = conf.counters.size - 1
+		Cat(conf.counters.map{ case(n,w) => {
+			val counter = Module(new SaturatingCounter(w))
+			counter.io.enable := Mux(connect_coverage, io.coverage_signals(left), false.B)
+			left = left - 1
+			counter.io.value
+		}}.toSeq)
+	}
+
+	val output_ii = RegInit(0.U(log2Ceil(output_count).W))
+	val output_next = io.valid && io.ready
+	output_ii := Mux(output_next, output_ii + 1.U, output_ii)
+	io.last := output_ii === (output_count - 1).U
+	val done = RegInit(false.B)
+	when(io.last && output_next) { done := true.B }
+	io.collect_done := done
+	io.valid := collecting && !done
+	io.data := MuxLookup(output_ii, 0.U, {
+		var left = coverage_width - 1
+		(0 until output_count).map{ case(ii) => ii.U -> {
+			val right = left - out_width + 1
+			val out = if(right >= 0) { coverage(left, right) } else {
+				Cat(coverage(left, 0), 0.U((-right).W)) }
+			left = left - out_width
+			out }}
+	})
+}
+
--- harness/src/harness.scala
+++ harness/src/harness.scala
@@ -6,27 +6,16 @@ import chisel3.util._
 import scala.collection.immutable.ListMap
 
 
+object div2Ceil {
+	def apply(dividend: Int, divisor: Int): Int = (dividend + divisor - 1) / divisor
+}
+
+// WARNING: do not edit! circuits are not designed to deal with different values!
 class AxisConfig {
 	val width = 64
-	val bit_count = (width + 7) / 8
+	val bit_count = div2Ceil(width, 8)
 }
 
-class SaturatingCounter(width : Int) extends Module {
-	val io = this.IO(new Bundle {
-		val enable = Input(Bool())
-		val value = Output(UInt(width.W))
-	})
-	val count = RegInit(0.U(width.W))
-	io.value := count
-	val max = ((1 << width) - 1).U
-	count := Mux(!io.enable || count === max, count, count + 1.U)
-}
-
-// class Coverage(conf: CoverageConfig) extends Module {
-// 	val io = this.IO(new ListBundle {
-// 		ListMap(""
-// 	})
-// }
 
 class Harness() extends Module {
 	val axis = new AxisConfig
@@ -43,20 +32,28 @@ class Harness() extends Module {
 		val m_axis_tdata = Output(UInt(axis.width.W))
 		val m_axis_tkeep = Output(UInt(axis.bit_count.W))
 		val m_axis_tlast = Output(Bool())
-	})
 
-	val dut = Module (new DUT (new DUTConfig (new CoverageConfig) ) )
+		// debug signals
+		val dbg_do_collect = Input(Bool())
+	})
+	val cov_conf = new CoverageConfig
+	val dut_conf = new DUTConfig(cov_conf)
+	val dut = Module (new DUT(dut_conf) )
+
+	val cov = Module (new Coverage(cov_conf))
+	cov.io.coverage_signals := dut.io.coverage
+	cov.io.do_collect := io.dbg_do_collect
+	io.m_axis_tvalid := cov.io.valid
+	io.m_axis_tdata  := cov.io.data
+	io.m_axis_tkeep  := ((1 << axis.bit_count) - 1).U
+	io.m_axis_tlast  := cov.io.last
+	cov.io.ready     := io.m_axis_tready
 
 	// do NOT accept any data from producer
 	io.s_axis_tready := false.B
 
-	// produce constant value
-	io.m_axis_tvalid := true.B
-	//io.m_axis_tdata := "h19931993".U
-	io.m_axis_tdata := dut.io.coverage
-	io.m_axis_tkeep := ((1 << axis.bit_count) - 1).U
-	// TODO: tlast @ true means that DMA will only copy one value ....
-	io.m_axis_tlast := true.B
+
+
 }
 
 object HarnessGenerator extends App {
