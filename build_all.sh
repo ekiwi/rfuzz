59fafee14a2f3084d9d72b74fa4758f78c4ada76
Kevin Laeufer <ekiwi@berkeley.edu>
2017-12-22 17:42:22 +0100

    [test] add build_all.sh script for very simple regression tests

---
+++ build_all.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+echo "WARN: run git clean -dxf first."
+
+echo "1) Instrumenting the GCD circuit"
+sbt "run-main firrtl.Driver -i example/gcd.fir -o gcd.v -X verilog -fct hardwareafl.firrtltransforms.AutoCoverage,hardwareafl.firrtltransforms.SplitMuxConditions -ll info"
+
+echo "2) Testing the Chisel Harness"
+cd harness
+sbt run
+cd ..
+
+echo "3) Generating the FPGA and Verilator Harness"
+cd harness
+sbt run
+cd ..
+
+echo "4) Building the Verilator Harness"
+cd verilator
+mkdir -p build
+cd build
+meson .. --buildtype=release
+ninja
+cd ..
+cd ..
+
+echo "5) Synthesizing the FPGA Circuit"
+cd fpga
+make clean
+make
+cd ..
