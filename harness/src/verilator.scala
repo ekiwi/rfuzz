0aec05b15ccc7e45c354a9cf19936ec9cec4adab
Kevin Laeufer <ekiwi@berkeley.edu>
2018-01-05 21:38:20 +0100

    [harness] verilator: calculate bytes needed for coverage and inputs

--- harness/src/verilator.scala
+++ harness/src/verilator.scala
@@ -10,16 +10,27 @@ import chisel3.util._
 
 
 class VerilatorHarness(dut_conf: DUTConfig) extends Module {
+	// 64bit alignment
+	val word_byte_count = 8
+	def bits_to_words(bits: Int) = div2Ceil(div2Ceil(bits, 8), word_byte_count)
+	def normalize_to_bytes(bits: Int) = bits_to_words(bits) * word_byte_count
+
+	val coverage_bits = dut_conf.coverageCounters.map{ case(_,w) => w }.reduce(_+_)
+	val input_byte_count = normalize_to_bytes(dut_conf.inputBits)
+	val coverage_byte_count = normalize_to_bytes(coverage_bits)
+
+	println(s"input_byte_count: $input_byte_count")
+	println(s"coverage_byte_count: $coverage_byte_count")
 
 	val io = this.IO(new Bundle {
-		val input_bytes = Input(Vec(16, UInt(8.W)))
-		val coverage_bytes = Output(Vec(16, UInt(8.W)))
+		val input_bytes = Input(Vec(input_byte_count, UInt(8.W)))
+		val coverage_bytes = Output(Vec(coverage_byte_count, UInt(8.W)))
 	})
 	val dut = Module(new DUT(dut_conf))
 
 	// inputs
 	val input_bytes = Cat(io.input_bytes)
-	val input_width = 16 * 8
+	val input_width = input_byte_count * 8
 	dut.io.inputs := input_bytes(input_width-1, input_width - dut_conf.inputBits)
 
 	// coverage
@@ -33,7 +44,7 @@ class VerilatorHarness(dut_conf: DUTConfig) extends Module {
 		}}.toSeq)
 	}
 	// TODO: clean up code
-	var left = dut_conf.coverageCounters.map{ case(_,w) => w }.reduce(_+_) - 1
+	var left = coverage_bits - 1
 	io.coverage_bytes.map{ case(cov) =>
 		val right = left - 8 + 1
 		cov := { if(right >= 0) {        coverage(left, right)
