6fb527da048d539e50f1c6e9e7767de28d24aa62
Kevin Laeufer <ekiwi@berkeley.edu>
2018-02-26 19:36:07 -0800

    [instrumentation] name files after classes contained in them
    The is no AutoCoverage transform anymore and the
    continued existence of AutoCoverage.scala was getting
    confusing.

--- instrumentation/src/main/scala/hardwareafl/firrtltransforms/TomlGenerator.scala
+++ instrumentation/src/main/scala/hardwareafl/firrtltransforms/TomlGenerator.scala
@@ -16,47 +16,6 @@ import java.util.Date
 
 import scala.collection.mutable
 
-// Removes compound expressions from mux conditions
-// Ensures they are a reference
-// Borrows from Firrtl's SplitExpressions
-class SplitMuxConditions extends Transform {
-  def inputForm = MidForm
-  def outputForm = MidForm
-
-  private def isRef(expr: Expression): Boolean = expr match {
-    case ref @ (_: WRef | _: WSubField | _: WSubIndex) => true
-    case _ => false
-  }
-
-  private def onModule(mod: Module): Module = {
-    val namespace = Namespace(mod)
-    def onStmt(s: Statement): Statement = {
-      val stmts = mutable.ArrayBuffer.empty[Statement]
-      def onExpr(e: Expression): Expression = e map onExpr match {
-        case mux @ Mux(cond, tval, fval, mtpe) if !isRef(cond) =>
-          val n = DefNode(Utils.get_info(s), namespace.newTemp, cond)
-          stmts.append(n)
-          Mux(WRef(n.name, cond.tpe, NodeKind, MALE), tval, fval, mtpe)
-        case mux: Mux =>
-          mux
-        case other => other
-      }
-
-      stmts += s.map(onExpr).map(onStmt)
-      stmts.size match {
-        case 1 => stmts.head
-        case _ => Block(stmts)
-      }
-    }
-    mod.copy(body = onStmt(mod.body))
-  }
-  def execute(state: CircuitState): CircuitState = {
-    state.copy(circuit = state.circuit.copy(modules = state.circuit.modules map {
-      case mod: Module => onModule(mod)
-      case ext: ExtModule => ext
-    }))
-  }
-}
 
 // TODO: this is realtively decoupled from the instrumentation pass
 //       -> maybe move into separate file
