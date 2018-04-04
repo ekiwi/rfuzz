07e7d7b937d4db8afba9010d9026ad5908b277dc
Kevin Laeufer <ekiwi@berkeley.edu>
2018-04-03 18:53:17 -0700

    [analysis] only disassmble sodor

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -12,16 +12,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from format import *
+import riscv
 
 def shell(): from IPython import embed; embed()
-
-dasm_bin = None if 'RISCV' not in os.environ else os.path.join(os.environ['RISCV'], 'bin', 'spike-dasm')
-
-def dasm(instruction):
-	hx = "DASM({:x})\n".format(instruction).encode('ASCII')
-	rr = subprocess.run([dasm_bin], input=hx, check=True, stdout=subprocess.PIPE)
-	return rr.stdout.decode('ASCII').strip()
-
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
@@ -29,29 +22,11 @@ if __name__ == '__main__':
 	parser.add_argument('DIR', help='fuzzer output directory to be analyzed', nargs=1)
 	args = parser.parse_args()
 
-	config, entries = load_results(args.DIR[0])
+	inp_dir = args.DIR[0]
+	config, entries = load_results(inp_dir)
 
-
-
-	fmt = InputFormat(config)
-
-	for ee in entries:
-		inp = Input(ee, fmt)
-		print("-------------------------------------------")
-		print("- {} Entry:".format(inp.id))
-		# for ii, cc, bb, raw in zip(range(inp.cycles), inp.formated, inp.bytes, inp.inputs):
-		# 	print("{}: {}".format(ii, raw))
-		# 	print("{}: {}".format(ii, bb))
-		# 	print("{}: {}".format(ii, cc))
-		for ii, cc in enumerate(inp.formated):
-			valid = cc['io_imem_resp_valid']
-			#skip = (valid == 0)
-			skip = False
-			if not skip:
-				binary = cc['io_imem_resp_bits_data']
-				instruction = dasm(binary)
-				print("{}: ({}) {:08x} -> {}".format(ii, valid, binary, instruction))
-
+	if os.path.basename(inp_dir).startswith("sodor"):
+		riscv.print_instructions(config, entries)
 
 	disco_times = [parse_time(entry['entry']['discovered_after']) for entry in entries]
 	plt.plot(disco_times, range(len(disco_times)) )
---
+++ analysis/riscv.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, University of California, Berkeley
+# author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+
+import os, subprocess
+from format import InputFormat, Input
+
+dasm_bin = None if 'RISCV' not in os.environ else os.path.join(os.environ['RISCV'], 'bin', 'spike-dasm')
+
+def dasm(instruction):
+	hx = "DASM({:x})\n".format(instruction).encode('ASCII')
+	rr = subprocess.run([dasm_bin], input=hx, check=True, stdout=subprocess.PIPE)
+	return rr.stdout.decode('ASCII').strip()
+
+def print_instructions(config, entries):
+	fmt = InputFormat(config)
+	for ee in entries:
+		inp = Input(ee, fmt)
+		print("-------------------------------------------")
+		print("- {} Entry:".format(inp.id))
+		# for ii, cc, bb, raw in zip(range(inp.cycles), inp.formated, inp.bytes, inp.inputs):
+		# 	print("{}: {}".format(ii, raw))
+		# 	print("{}: {}".format(ii, bb))
+		# 	print("{}: {}".format(ii, cc))
+		for ii, cc in enumerate(inp.formated):
+			valid = cc['io_imem_resp_valid']
+			#skip = (valid == 0)
+			skip = False
+			if not skip:
+				binary = cc['io_imem_resp_bits_data']
+				instruction = dasm(binary)
+				print("{}: ({}) {:08x} -> {}".format(ii, valid, binary, instruction))
