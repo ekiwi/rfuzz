a141fe43ba7e695b9d99bda93bfde93feb6610fd
Kevin Laeufer <ekiwi@berkeley.edu>
2018-04-05 23:05:48 -0700

    [analysis] work with multiple directories

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -19,24 +19,33 @@ def shell(): from IPython import embed; embed()
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		description='analysis script for the fuzzing results')
-	parser.add_argument('DIR', help='fuzzer output directory to be analyzed', nargs=1)
+	parser.add_argument('DIR', help='fuzzer output directory to be analyzed', nargs='+')
 	args = parser.parse_args()
 
-	inp_dir = args.DIR[0]
-	config, entries = load_results(inp_dir)
+	coverage_data = []
 
-	#if os.path.basename(inp_dir).startswith("sodor"):
-	#	riscv.print_instructions(config, entries)
+	for inp_dir in args.DIR:
+		name = os.path.basename(inp_dir)
+		config, entries = load_results(inp_dir)
 
-	cov = CoverageFormat(config)
-	fmt = InputFormat(config)
-	inputs = [Input(ee, fmt, cov) for ee in entries]
+		#if os.path.basename(inp_dir).startswith("sodor"):
+		#	riscv.print_instructions(config, entries)
 
-	make_mutation_graph("mutations.png", inputs)
+		cov = CoverageFormat(config)
+		fmt = InputFormat(config)
+		inputs = [Input(ee, fmt, cov) for ee in entries]
 
-	disco_times = [ii.discovered_after for ii in inputs]
-	cov = [ii.total_cov / ii.max_cov for ii in inputs]
-	plt.plot(disco_times, cov)
+		make_mutation_graph("{}_mutations.png".format(name), inputs)
+
+		disco_times = [ii.discovered_after for ii in inputs]
+		cov = [ii.total_cov / ii.max_cov for ii in inputs]
+		coverage_data.append((disco_times, cov))
+
+	max_time = max(cc[0][-1] for cc in coverage_data)
+	for disco_times, cov in coverage_data:
+		disco_times.append(max_time)
+		cov.append(cov[-1])
+		plt.plot(disco_times, cov)
 	plt.ylabel("T/F Coverage")
 	plt.xlabel("Time (s)")
 	plt.show()
