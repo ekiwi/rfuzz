2d8368805627c46b8d4c2d0b10f26ef9cf58e0ab
Kevin Laeufer <ekiwi@berkeley.edu>
2018-04-11 17:15:07 -0700

    [analysis] e2e: make independent of fuzzer coverage code

--- analysis/e2e.py
+++ analysis/e2e.py
@@ -11,7 +11,7 @@
 
 import os, sys, toml, tempfile, subprocess, json
 
-from format import CoverageFormat
+from format import to_bytes
 
 analysis_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.abspath(os.path.join(analysis_dir, '..', 'build'))
@@ -29,26 +29,25 @@ class CoverageCalcuator:
 	""" stateful wrapper around the coverage oracle """
 	def __init__(self, dut):
 		self.oracle = CoverageOracle.get(dut)
-		assert self.oracle.fmt.tf_coverage
+		self.counters = self.oracle.counters
+		# initialize coverage state
 		self.union = set() # index set
 	def get(self, inp):
 		trace_bits = self.oracle.query(inp)
-		assert len(trace_bits) == self.oracle.fmt.bytes
-		if len(trace_bits) > len(self.oracle.fmt.counters):
-			trace_bits = trace_bits[:len(self.oracle.fmt.counters)-len(trace_bits)]
-		assert len(trace_bits) == len(self.oracle.fmt.counters)
+		assert len(trace_bits) == self.oracle.coverage_size
+		if len(trace_bits) > len(self.counters):
+			trace_bits = trace_bits[:len(self.counters)-len(trace_bits)]
+		assert len(trace_bits) == len(self.counters)
 		covered = bin(trace_bits)
 		new_coverage = covered - self.union
 		self.union = covered | self.union
 		return {
 			# possible values per (TF) counter are: 01, 10, 11
-			'max': len(self.oracle.fmt.counters) * 3,
+			'max': len(self.counters) * 3,
 			'total': len(self.union),
 			'local': len(covered),
 			'new': len(new_coverage),
 		}
-
-
 
 class CoverageOracle:
 	_oracles = {}
@@ -64,7 +63,14 @@ class CoverageOracle:
 		cov_bin  = os.path.join(build_dir, "{}_cov".format(dut))
 		assert os.path.isfile(cov_bin)
 		config = toml.loads(open(cov_toml).read())
-		self.fmt = CoverageFormat(config)
+		# make sure the coverage instrumentation is the way we want
+		self.counters = config['counter']
+		assert all(counter['index'] == ii for ii, counter in enumerate(self.counters))
+		assert all(counter['width'] == 8 for counter in self.counters)
+		assert all(cc['name'] == 'TF' for cc in self.counters)
+		# calculate coverage size
+		bits = sum(cc['width'] for cc in self.counters)
+		self.coverage_size = to_bytes(bits + 2 * 8) - 2
 		self._bin = cov_bin
 	def query(self, inp):
 		assert all(isinstance(cc, bytes) for cc in inp)
