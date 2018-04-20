9e912021a66308a13da0f9f76c7bcd1ba1f7723f
Kevin Laeufer <ekiwi@berkeley.edu>
2018-04-20 12:34:44 -0700

    [instrumentation] test: 2 entry sparse mem should be able to accept 2 writes

--- instrumentation/src/test/scala/hardwareafl/SparseMemSpec.scala
+++ instrumentation/src/test/scala/hardwareafl/SparseMemSpec.scala
@@ -34,6 +34,35 @@ class SparseMemTester extends BasicTester {
   }
 }
 
+class SparseMemMaxWritesTester extends BasicTester {
+  val depth = 2
+  val addrWidth = 2
+  val mod = Module(new SparseMem(UInt(32.W), depth, addrWidth, 1, 1, false))
+  // Defaults
+  mod.io <> DontCare
+  mod.io.w(0).en := false.B
+  mod.io.r(0).en := false.B
+
+  val (cycle, done) = Counter(true.B, 8)
+  printf(p" ***** Cycle $cycle *****\n")
+  printf(p"${mod.io}\n")
+  when (cycle === 0.U) {
+    mod.io.w(0).write(1.U, 123.U)
+  }
+  .elsewhen(cycle === 1.U) {
+    mod.io.w(0).write(1.U, 123.U)
+  }
+  .elsewhen(cycle === 2.U) {
+    // this should not lead to an assertion violation since the memory can hold
+    // two entries!
+    mod.io.w(0).write(0.U, 123.U)
+  }
+  when (done) {
+    stop()
+  }
+}
+
 object TestSparseMem extends App {
   assert(TesterDriver.execute(() => new SparseMemTester), "Test must work!")
+  assert(TesterDriver.execute(() => new SparseMemMaxWritesTester), "No assertion should fire!")
 }
