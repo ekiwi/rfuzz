b796d159e10a89f44456c8a7d69254b3c7dae4b4
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-05-09 19:58:09 -0700

    Revert "fix meson for newer verilator version"
    This reverts commit 9eabea6a70c7d9a4a511696e32f3391e0caf4251.

--- verilator/verilator.py
+++ verilator/verilator.py
@@ -13,7 +13,7 @@ DefaultFlags = ['--assert', '-Wno-fatal', '-Wno-WIDTH', '-Wno-STMTDLY',
                 OptLevel, '-CFLAGS', '"{}"'.format(' '.join(CFlags))]
 VerilatorIncluder = os.path.join('bin', 'verilator_includer')
 # File Prediction Settings
-GeneratedFiles = ['V{}_classes.mk', 'V{}.cpp', 'V{}.h', 'V{}.mk', 'V{}__Slow.cpp',
+GeneratedFiles = ['V{}_classes.mk', 'V{}.cpp', 'V{}.h', 'V{}.mk',
                   'V{}__Syms.cpp', 'V{}__Syms.h', 'V{}__ver.d', 'V{}__verFiles.dat']
 MaybeGeneratedFiles = ['V{}__Inlines.h']
 GeneratedTraceFiles = ['V{}__Trace.cpp', 'V{}__Trace__Slow.cpp']
